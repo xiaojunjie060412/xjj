576b70a683bdee2eaedeec352063af374b50ae16
xjj <1309376598@qq.com>
2019-06-06 11:42:38 +0800

    解除硬编码

--- demo1/booktest/urls.py
+++ demo1/booktest/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from .views import index, list, detail
+app_name = 'booktest'
 
 urlpatterns = [
-    url(r'^index/$', index),
-    url(r'^list/$', list),
-    url(r'^detail/(\d+)/$', detail),
+
+    url(r'^list/$', list, name='list'),
+    url(r'^detail/(\d+)/$', detail, name='detail'),
+    url(r'^$', index, name='index'),
 ]
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -7,23 +7,26 @@ from .models import BookInfo, HeroInfo
 
 
 def index(request):
-    temp = loader.get_template('booktest/index.html')
-    res = temp.render({})
-
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/index.html')
+    # res = temp.render({})
+    #
+    # return HttpResponse(res)
+    return render(request, 'booktest/index.html', {})
 
 
 def list(request):
     books = BookInfo.objects.all()
-    temp = loader.get_template('booktest/list.html')
-    res = temp.render({'books': books})
-
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/list.html')
+    # res = temp.render({'books': books})
+    #
+    # return HttpResponse(res)
+    return render(request, 'booktest/list.html', {'books': books})
 
 
 def detail(request, id):
     book = BookInfo.objects.get(pk=id)
-    temp = loader.get_template('booktest/detail.html')
-    res = temp.render({'book': book})
-
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/detail.html')
+    # res = temp.render({'book': book})
+    #
+    # return HttpResponse(res)
+    return render(request, 'booktest/detail.html', {'book': book})
