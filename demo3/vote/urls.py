e22a494147af0afe625985686a99eddc34397e2d
xjj <1309376598@qq.com>
2019-06-11 20:18:56 +0800

    添加登录界面

--- demo3/vote/urls.py
+++ demo3/vote/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import ListViews, DetailViews, ResultViews
+from .views import ListViews, DetailViews, ResultViews, LoginViews
 app_name = 'vote'
 
 urlpatterns = [
     url(r'^list/$', ListViews.as_view(), name='list'),
     url(r'^detail/(\d+)/$', DetailViews.as_view(), name='detail'),
     url(r'^result/(\d+)/$', ResultViews.as_view(), name='result'),
+    url(r'^login/$', LoginViews.as_view(), name='login'),
 ]
--- demo3/vote/views.py
+++ demo3/vote/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import Problem, Answer
 from django.views.generic import View
 # Create your views here.
 
 
+def checklogin(fun):
+    def check(self, req, *args):
+        # if req.COOKIES.get("username"):
+        if req.session.get('username'):
+            return fun(self, req, *args)
+        else:
+            return redirect(reverse("vote:login"))
+    return check
+
+
 class ListViews(View):
 
+    @checklogin
     def get(self, req):
         problems = Problem.objects.all()
         return render(req, 'vote/list.html', locals())
@@ -14,10 +25,12 @@ class ListViews(View):
 
 class DetailViews(View):
 
+    @checklogin
     def get(self, req, id):
         problem = Problem.objects.get(pk=id)
         return render(req, 'vote/detail.html', locals())
 
+    @checklogin
     def post(self, req, id):
         a_id = req.POST.get('res')
         a = Answer.objects.get(pk=a_id)
@@ -28,6 +41,22 @@ class DetailViews(View):
 
 class ResultViews(View):
 
+    @checklogin
     def get(self, req, id):
         problem = Problem.objects.get(pk=id)
         return render(req, 'vote/result.html', locals())
+
+
+class LoginViews(View):
+
+    def get(self, req):
+        return render(req, 'vote/login.html')
+
+    def post(self, req):
+        username = req.POST.get('username')
+        password = req.POST.get('password')
+        # res = redirect(reverse('vote:list'))
+        # res.set_cookie('username', username)
+        # return res
+        req.session['username'] = username
+        return redirect(reverse('vote:list'))
